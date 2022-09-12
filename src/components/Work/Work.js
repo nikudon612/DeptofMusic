2b3de062c3f5788a8459cadac8adf57c3758963e
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-09-11 20:26:10 -0400

    Changed team photos and added css updates for those assets

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Work.scss";
+import Nationwide from "../../Assets/Videos/nationwide.mov";
 
 const Work = () => {
   return (
@@ -7,32 +8,49 @@ const Work = () => {
       <div className="grid">
         <div className="one">
           <div className="full">
-            full
-            <img />
+            <video width="100%" height="100%" controls>
+              <source src={Nationwide} type="video/mp4" />
+            </video>
+            {/* <iframe
+              title="full video"
+              src="https://www.youtube.com/embed/bgr4sGq2ba0?autoplay=0"
+              frameborder="0"
+              width="100%"
+              height="100%"
+            ></iframe> */}
           </div>
         </div>
         <div className="two">
-          <div className="half">half</div>
-          <div className="half">half</div>
+          <div className="half">
+            <iframe
+              title="half video"
+              src="https://www.youtube.com/embed/bgr4sGq2ba0"
+              width="100%"
+              height="100%"
+            ></iframe>
+          </div>
+          <div className="half">
+            <iframe
+              //   title="half video"
+              src="https://player.vimeo.com/video/78421150?h=8aa689e131&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+              frameborder="0"
+              width="100%"
+              height="500px"
+            ></iframe>
+          </div>
         </div>
         <div className="three">
-          <div className="third">third</div>
+          <div className="third">
+            <iframe
+              title="third video"
+              src="https://www.youtube.com/embed/bgr4sGq2ba0"
+              width="100%"
+              height="100%"
+            ></iframe>
+          </div>
           <div className="third">third</div>
           <div className="third">third</div>
         </div>
-        {/* <div className="two">
-          <div className="half">half</div>
-          <div className="half">half</div>
-        </div> */}
-        {/* <div className="three">
-          <div className="third">third</div>
-          <div className="third">third</div>
-          <div className="third">third</div>
-        </div>
-        <div className="two">
-          <div className="half">half</div>
-          <div className="half">half</div>
-        </div> */}
       </div>
     </div>
   );
