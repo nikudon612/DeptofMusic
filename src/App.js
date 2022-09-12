4fcd3558d7344feb61474b9c2ba4280abbdf9b45
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-09-11 23:59:48 -0400

    Added camp font to whole site

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Team from "./components/Team/Team";
 import Footer from "./components/Footer/Footer";
-import "./Assets/Fonts/TrueNorth-Regular.otf";
 
 function App() {
   return (
