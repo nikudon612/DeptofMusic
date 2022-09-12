5133e25ccbd0a7e3802cfd520e9f2a46bb93f330
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-09-12 19:57:09 -0400

    Added react scroll package and functionality to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Navbar.scss";
 import Eagle from "../../Assets/DoM-BrandAssets_LogoMark-V1.png";
 import Logo from "../..//Assets/DoM-BrandAssets_Logo1-Wordmark-Black.png";
+import { Link } from "react-scroll";
+
+// import MenuIcon from "@material-ui/icons/Menu";
 // import FullLogo from "../../Assets/DOMHeaderLogo.png";
 
 const Navbar = () => {
@@ -15,13 +18,20 @@ const Navbar = () => {
         </a>
       </div>
       <div className="links">
-        <a href="/work">Work</a>
+        <Link to="work" smooth={true} duration={750}>
+          Work
+        </Link>
+        <Link to="about" smooth={true} duration={750}>
+          About
+        </Link>
+        <Link to="contact" smooth={true} duration={750}>
+          Contact
+        </Link>
+        {/* <a href="/work">Work</a>
         <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <a href="/contact">Contact</a> */}
       </div>
-      <div className="hamburger">
-        <button>X</button>
-      </div>
+      <div className="hamburger">{/* <MenuIcon /> */}</div>
     </div>
   );
 };
