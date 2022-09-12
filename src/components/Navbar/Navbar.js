8ef2dc0111a9b463b594de4531480a935901e0cd
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-09-12 14:57:48 -0400

    created grid template for work section w hover text animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./Navbar.scss";
 import Eagle from "../../Assets/DoM-BrandAssets_LogoMark-V1.png";
 import Logo from "../..//Assets/DoM-BrandAssets_Logo1-Wordmark-Black.png";
-<link rel="stylesheet" href="https://use.typekit.net/fzf4moa.css"></link>;
+// import FullLogo from "../../Assets/DOMHeaderLogo.png";
+
 const Navbar = () => {
   return (
     <div className="navbar">
@@ -10,6 +11,7 @@ const Navbar = () => {
         <a href="/" className="logoLinks">
           <img className="eagle" src={Eagle} alt="Dept. of Music Eagle Logo" />
           <img className="logo" src={Logo} alt="Dept. of Music Text Logo" />
+          {/* <img className="logo" src={FullLogo} alt="Dept. of Music Text Logo" /> */}
         </a>
       </div>
       <div className="links">
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,27 +1,66 @@
 import React from "react";
 import "./Work.scss";
-// import Nationwide from "../../Assets/Videos/nationwide.mov";
+import Nationwide from "../../Assets/Nationwide.png";
+import Absolut from "../../Assets/absolut.png";
+import Zippo from "../../Assets/zipp.png";
+import Barb from "../../Assets/barbarian.png";
 
 const Work = () => {
   return (
     <div className="work">
       <div className="grid">
         <div className="one">
-          <div className="full">Full</div>
+          <div className="full">
+            {/* <img src={Zippo} alt="nationwide thumbnail" />
+            <div className="project-desc-full">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div> */}
+            <img src={Zippo} alt="nationwide thumbnail" />
+            <div className="project-desc">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div>
+          </div>
         </div>
         <div className="two">
           <div className="half">
-            <a href="/">
-              <img className="static" src="" />
-              <img className="active" src="" />
-            </a>
+            <img src={Nationwide} alt="nationwide thumbnail" />
+            <div className="project-desc">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div>
           </div>
-          <div className="half">half</div>
+          <div className="half">
+            <img src={Absolut} alt="nationwide thumbnail" />
+            <div className="project-desc">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div>
+          </div>
         </div>
         <div className="three">
-          <div className="third">third</div>
-          <div className="third">third</div>
-          <div className="third">third</div>
+          <div className="third">
+            <img src={Barb} alt="nationwide thumbnail" />
+            <div className="project-desc">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div>
+          </div>
+          <div className="third">
+            <img src={Barb} alt="nationwide thumbnail" />
+            <div className="project-desc">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div>
+          </div>
+          <div className="third">
+            <img src={Barb} alt="nationwide thumbnail" />
+            <div className="project-desc">
+              <p>Track Title</p>
+              <p>Project Info</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
