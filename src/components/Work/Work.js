85cd2dd3e3192cdeb80e8af7b465778f1a2012a0
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-09-24 17:06:21 -0400

    Half modal is showing unique videos when clicked

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Work.scss";
 import Nationwide from "../../Assets/Nationwide.png";
 import Absolut from "../../Assets/absolut.png";
 import Zippo from "../../Assets/zipp.png";
 import Barb from "../../Assets/barbarian.png";
+import { Modal } from "@mui/material";
+import Box from "@mui/material/Box";
+import FullModal from "../Modals/FullModal";
 
 const Work = () => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
   //Project Information Objects//
   const featuredProject = {
     id: 1,
@@ -21,6 +35,7 @@ const Work = () => {
       title: "Nationwide theme by H.E.R.",
       info: "Unique rendition of the Nationwide theme by H.E.R.",
       image: Nationwide,
+      link: "https://drive.google.com/file/d/1yelACSoUs3uocnp-0LzZrWDB7SCNc59L/preview",
     },
     {
       id: 2,
@@ -28,6 +43,7 @@ const Work = () => {
       title: "Absolut",
       info: "We did x, y and z",
       image: Absolut,
+      link: "https://drive.google.com/file/d/17-LWxSpoCa-gONISwRKlgfFRK3mC2Ur_/preview",
     },
   ];
 
@@ -56,6 +72,12 @@ const Work = () => {
   ];
 
   //Project Information Objects//
+  //Modal State//
+  const [open, setOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  //Modal State//
 
   return (
     <div className="work">
@@ -71,7 +93,14 @@ const Work = () => {
         </div>
         <div className="two">
           {halfProjects.map((project) => (
-            <div className="half">
+            <div
+              className="half"
+              key={project.id}
+              onClick={() => {
+                setModalData(project.link);
+                setOpen(true);
+              }}
+            >
               <img src={project.image} alt="thumbnail" />
               <div className="project-desc">
                 <p className="project-title">{project.client}</p>
@@ -79,10 +108,28 @@ const Work = () => {
               </div>
             </div>
           ))}
+          {/* Video Modal */}
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <iframe
+                src={modalData}
+                width="640"
+                height="480"
+                allow="autoplay"
+                title="nationwide"
+              ></iframe>
+            </Box>
+          </Modal>
+          {/* Video Modal */}
         </div>
         <div className="three">
           {threeProjects.map((project) => (
-            <div className="third">
+            <div className="third" key={project.id}>
               <img src={project.image} alt="thumbnail" />
               <div className="project-desc">
                 <p className="project-title">{project.client}</p>
