957c9725af5bd81b32c00d694d7d6187645e14c1
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-09-24 17:19:19 -0400

    rendering unique videos on modal + styling changes

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -5,27 +5,16 @@ import Absolut from "../../Assets/absolut.png";
 import Zippo from "../../Assets/zipp.png";
 import Barb from "../../Assets/barbarian.png";
 import { Modal } from "@mui/material";
-import Box from "@mui/material/Box";
 import FullModal from "../Modals/FullModal";
 
 const Work = () => {
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
   //Project Information Objects//
   const featuredProject = {
     id: 1,
     client: "Zippo",
     title: "Zippo",
     info: "We did x, y and z",
+    link: "https://drive.google.com/file/d/13Pr8XVdciBgaJhI8yAcTH9WZxsrDGgx1/preview",
   };
 
   const halfProjects = [
@@ -54,6 +43,7 @@ const Work = () => {
       title: "Samsung Galaxy Z Fold3 5G",
       info: "We did x, y and z",
       image: Barb,
+      link: "https://drive.google.com/file/d/1Kt_zlgHEPYMfDzKzmuhXQR2zzIbQnGKL/preview",
     },
     {
       id: 2,
@@ -61,6 +51,7 @@ const Work = () => {
       title: "Nationwide theme by H.E.R.",
       info: "Unique rendition of the Nationwide theme by H.E.R.",
       image: Nationwide,
+      link: "https://drive.google.com/file/d/1yelACSoUs3uocnp-0LzZrWDB7SCNc59L/preview",
     },
     {
       id: 3,
@@ -68,22 +59,52 @@ const Work = () => {
       title: "Absolut",
       info: "We did x, y and z",
       image: Absolut,
+      link: "https://drive.google.com/file/d/17-LWxSpoCa-gONISwRKlgfFRK3mC2Ur_/preview",
     },
   ];
-
   //Project Information Objects//
+
   //Modal State//
   const [open, setOpen] = useState(false);
   const [modalData, setModalData] = useState(null);
-  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   //Modal State//
 
   return (
     <div className="work">
+      {/* Video Modal */}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <iframe
+          src={modalData}
+          width="640"
+          height="480"
+          allow="autoplay"
+          title="nationwide"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            outline: 0,
+            border: "none",
+          }}
+        ></iframe>
+      </Modal>
+      {/* Video Modal */}
       <div className="grid">
         <div className="one">
-          <div className="full">
+          <div
+            className="full"
+            onClick={() => {
+              setModalData(featuredProject.link);
+              setOpen(true);
+            }}
+          >
             <img src={Zippo} alt="featured thumbnail" />
             <div className="project-desc">
               <p className="project-title">{featuredProject.client}</p>
@@ -108,28 +129,17 @@ const Work = () => {
               </div>
             </div>
           ))}
-          {/* Video Modal */}
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <iframe
-                src={modalData}
-                width="640"
-                height="480"
-                allow="autoplay"
-                title="nationwide"
-              ></iframe>
-            </Box>
-          </Modal>
-          {/* Video Modal */}
         </div>
         <div className="three">
           {threeProjects.map((project) => (
-            <div className="third" key={project.id}>
+            <div
+              className="third"
+              key={project.id}
+              onClick={() => {
+                setModalData(project.link);
+                setOpen(true);
+              }}
+            >
               <img src={project.image} alt="thumbnail" />
               <div className="project-desc">
                 <p className="project-title">{project.client}</p>
