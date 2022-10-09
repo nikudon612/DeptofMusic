083d16642d089dbd3760225cbe4938b1d2fe3191
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-10-09 13:04:26 -0400

    Added framer motion library, added load animation for text elements

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,14 +4,39 @@ import Freeman from "../../Assets/DoM-Oscar-HiRes.png";
 import Kaplan from "../../Assets/DoM-BrandAssets-Walrus-32.png";
 import { Link } from "react-scroll";
 import ExpandCircleDownIcon from "@mui/icons-material/ExpandCircleDown";
+import { motion } from "framer-motion";
 
 const Team = () => {
   return (
     <div className="team">
       <div className="team-outer-outer-container">
-        <div className="team-title">Our Team</div>
+        <motion.div
+          className="team-title"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, scale: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            bounce: 0.3,
+            duration: 2,
+            delay: 0.1,
+            ease: "linear",
+          }}
+        >
+          Our Team
+        </motion.div>
         <div className="team-outer-container">
-          <div className="team-inner-container">
+          <motion.div
+            className="team-inner-container"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, scale: 1, y: 0 }}
+            transition={{
+              type: "spring",
+              bounce: 0.3,
+              duration: 2,
+              delay: 0.4,
+              ease: "linear",
+            }}
+          >
             <div className="team-container">
               <div className="team-info">
                 <span className="employee">Michael Freeman</span>
@@ -21,9 +46,20 @@ const Team = () => {
             <div className="image">
               <img src={Freeman} alt="Oscar the Pup logo" />
             </div>
-          </div>
+          </motion.div>
           <div className="team-outer-container">
-            <div className="team-inner-container">
+            <motion.div
+              className="team-inner-container"
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, scale: 1, y: 0 }}
+              transition={{
+                type: "spring",
+                bounce: 0.3,
+                duration: 2,
+                delay: 0.8,
+                ease: "linear",
+              }}
+            >
               <div className="team-container">
                 <div className="team-info">
                   <span className="employee">Robert Kaplan</span>
@@ -34,7 +70,7 @@ const Team = () => {
               <div className="image">
                 <img src={Kaplan} alt="Walrus logo" className="walrus" />
               </div>
-            </div>
+            </motion.div>
             <Link to="navbar" smooth={true} duration={1250}>
               <ExpandCircleDownIcon className="circle" />
             </Link>
