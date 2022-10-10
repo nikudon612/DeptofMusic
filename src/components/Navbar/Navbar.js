ca06c3cb0cc7c26aeabb69dff0377a3a83961c29
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-10-10 19:07:02 -0400

    small commenting out error

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./Navbar.scss";
 // import Eagle from "../../Assets/DoM-Opt1.png";
 import Logo from "../../Assets/DoM-Nav-Opt.png";
-import HoverLogo from "../../Assets/DoM-Nav-Hover-Opt.png";
 import { Link } from "react-scroll";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
